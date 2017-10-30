974f37430f3b967b93e8404f947e1ea8aa65a9a9
Ian Perkins <ianperkins57@gmail.com>
2017-10-29 21:09:09 -0500

    hw8 progress (not finished)

--- HW8/homework8.cpp
+++ HW8/homework8.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -57,16 +58,63 @@ void readInputFile(char * inputFile, vector<Activity> &activities) {
 			}
 			if(temp.empty()) {
 				temp = activityValues[0];
-				activityValues.clear();
+			} else {
+				activities.push_back(Activity(activityValues[0], activityValues[1], activityValues[2], activityValues[3]));
 			}
-			activities.push_back(Activity(activityValues[0], activityValues[1], activityValues[2], activityValues[3]));
 			activityValues.clear();
 		}
 	}
 }
 
-void activitySelector() {
-	
+bool finishTimeSort(const Activity i, const Activity j) { return (i.endTime < j.endTime); }
+
+vector<pair<int, int> > weightedActivitySelector(vector<Activity> &activities) {
+	int numActivities = activities.size();
+	vector<pair<int, int> > weightedActivities(numActivities);
+
+	if(numActivities == 1) {
+		weightedActivities.push_back(make_pair(activities[0].id, activities[0].value));
+		return weightedActivities;
+	}
+
+	sort(activities.begin(), activities.end(), finishTimeSort);
+
+	weightedActivities[0] = make_pair(activities[0].id, activities[0].value);
+	int j;
+	pair<int, int> x, y;
+	for(int i = 1; i < numActivities; i++) {
+		x = make_pair(activities[i].id, activities[i].value);
+		for(j = i - 1; j >= 0 && activities[i].startTime <= activities[j].endTime; j--) { }
+		y = make_pair(0, 0);
+		if(j != -1) {
+			y = make_pair(activities[j].id, activities[j].value);
+		}
+		weightedActivities[i] = make_pair( ,max(x.second + y.second, weightedActivities[i - 1].second));
+	}
+
+	return weightedActivities;
+}
+
+int weightedActivity(int n, vector<activity> &a) {
+    if(n == 1) {
+        return a[0].v;
+    }
+    vector<int> dp(n);
+    dp[0] = a[0].v;
+    int x, y, i, j;
+    for(i=1; i<n; i++) {
+        x = a[i].v;
+        j = i - 1;
+        while(j>=0 && a[i].s<=a[j].e) {
+            j--;
+        }
+        y = 0;
+        if(j != -1) {
+            y = a[j].v;
+        }
+        dp[i] = max(x + y, dp[i-1]);
+    }
+    return dp[n-1];
 }
 
 int main(int argc, char * argv[]) {
@@ -81,8 +129,7 @@ int main(int argc, char * argv[]) {
  	vector<Activity> activities;
 
  	readInputFile(inputFile, activities);
-
-	// testing ssh key 	
+ 	weightedActivitySelector(activities);
 
 	return 0;
 }
